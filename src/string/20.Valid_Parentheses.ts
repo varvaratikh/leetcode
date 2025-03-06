faa3aaf93eddf80528f1a626555584ed1e4d8593
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-03-06 09:21:38 +0300

    solved 20.Valid Parenthese

---
+++ src/string/20.Valid_Parentheses.ts
@@ -0,0 +1,54 @@
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+// An input string is valid if:
+//
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+//
+//
+// Example 1:
+//
+// Input: s = "()"
+//
+// Output: true
+//
+// Example 2:
+//
+// Input: s = "()[]{}"
+//
+// Output: true
+//
+// Example 3:
+//
+// Input: s = "(]"
+//
+// Output: false
+//
+// Example 4:
+//
+// Input: s = "([])"
+//
+// Output: true
+
+
+const isValid = (s: string): boolean => {
+    const stack: string[] = [];
+    const brackets = new Map([
+        [')', '('],
+        [']', '['],
+        ['}', '{']
+    ]);
+
+    for (let char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack.pop() !== brackets.get(char)) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
