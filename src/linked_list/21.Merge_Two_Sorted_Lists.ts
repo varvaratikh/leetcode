7be49b0bf9346fe87f1220c1223f714b58d06aee
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-03-10 10:07:05 +0300

    added

--- src/linked_list/21.Merge_Two_Sorted_Lists.ts
+++ src/linked_list/21.Merge_Two_Sorted_Lists.ts
@@ -1,58 +1,58 @@
-//You are given the heads of two sorted linked lists list1 and list2.
+// //You are given the heads of two sorted linked lists list1 and list2.
+// //
+// // Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// //
+// // Return the head of the merged linked list.
+// //
+// //
+// //
+// // Example 1:
+// //
+// //
+// // Input: list1 = [1,2,4], list2 = [1,3,4]
+// // Output: [1,1,2,3,4,4]
+// // Example 2:
+// //
+// // Input: list1 = [], list2 = []
+// // Output: []
+// // Example 3:
+// //
+// // Input: list1 = [], list2 = [0]
+// // Output: [0]
+// //
 //
-// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
 //
-// Return the head of the merged linked list.
+// /**
+//  * Definition for singly-linked list.
+//  * class ListNode {
+//  *     val: number
+//  *     next: ListNode | null
+//  *     constructor(val?: number, next?: ListNode | null) {
+//  *         this.val = (val===undefined ? 0 : val)
+//  *         this.next = (next===undefined ? null : next)
+//  *     }
+//  * }
+//  */
 //
+// const mergeTwoLists = (list1: ListNode | null, list2: ListNode | null): ListNode | null => {
+//     if (!list1) return list2;
+//     if (!list2) return list1;
 //
+//     let dummy = new ListNode();
+//     let current = dummy;
 //
-// Example 1:
+//     while (list1 && list2) {
+//         if (list1.val < list2.val) {
+//             current.next = list1;
+//             list1 = list1.next;
+//         } else {
+//             current.next = list2;
+//             list2 = list2.next;
+//         }
+//         current = current.next;
+//     }
 //
+//     current.next = list1 || list2;
 //
-// Input: list1 = [1,2,4], list2 = [1,3,4]
-// Output: [1,1,2,3,4,4]
-// Example 2:
-//
-// Input: list1 = [], list2 = []
-// Output: []
-// Example 3:
-//
-// Input: list1 = [], list2 = [0]
-// Output: [0]
-//
-
-
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
-const mergeTwoLists = (list1: ListNode | null, list2: ListNode | null): ListNode | null => {
-    if (!list1) return list2;
-    if (!list2) return list1;
-
-    let dummy = new ListNode();
-    let current = dummy;
-
-    while (list1 && list2) {
-        if (list1.val < list2.val) {
-            current.next = list1;
-            list1 = list1.next;
-        } else {
-            current.next = list2;
-            list2 = list2.next;
-        }
-        current = current.next;
-    }
-
-    current.next = list1 || list2;
-
-    return dummy.next;
-};
+//     return dummy.next;
+// };
